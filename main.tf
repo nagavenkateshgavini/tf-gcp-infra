7aaa9c406478d5ddb3e869ef4abee7cc12bfc246
Naga Venkatesh Gavini <nagavenkat9948@gmail.com>
2024-02-15 11:44:08 -0800

    use variables (#6)

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "google" {
 }
 
 resource "google_compute_network" "webapp_vpc" {
-  name = "webapp-vpc"
+  name = var.vpc_name
   auto_create_subnetworks = false
   routing_mode = "REGIONAL"
   delete_default_routes_on_create = true
@@ -21,21 +21,21 @@ resource "google_compute_network" "webapp_vpc" {
 
 resource "google_compute_subnetwork" "webapp" {
   ip_cidr_range = var.webapp_cidr_range
-  name          = "webapp"
+  name          = var.webapp_subnet_name
   network       = google_compute_network.webapp_vpc.id
   region        = var.region
 }
 
 resource "google_compute_subnetwork" "db" {
   ip_cidr_range = var.db_cidr_range
-  name          = "db"
+  name          = var.db_subnet_name
   network       = google_compute_network.webapp_vpc.id
   region        = var.region
 }
 
-resource "google_compute_route" "webapp_subnet_route" {
+resource "google_compute_route" "webapp_vpc_route" {
   dest_range = var.webapp_route
-  name = "webapp-route"
+  name = var.vpc_route_name
   network = google_compute_network.webapp_vpc.name
   next_hop_gateway = "default-internet-gateway"
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "project_id" {
   description = "Pass the Project Id during execution"
   type = string
+  default = "csye-project-413917"
 }
 
 variable "region" {
@@ -25,4 +26,24 @@ variable "webapp_route" {
   description = "cidr range for subnet route"
   type = string
   default = "0.0.0.0/0"
+}
+
+variable "vpc_name" {
+  description = "vpc name"
+  type = string
+}
+
+variable "webapp_subnet_name" {
+  description = "webapp_subnet_name"
+  type = string
+}
+
+variable "db_subnet_name" {
+  description = "db_subnet_name"
+  type = string
+}
+
+variable "vpc_route_name" {
+  description = "route name"
+  type = string
 }
